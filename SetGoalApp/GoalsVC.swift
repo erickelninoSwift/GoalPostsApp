608edf65f6c90f651ae9be7f5a3704ee10c29c09
Erick Tshimbombo <erick.tshimbombo@yahoo.com>
2020-09-04 16:50:49 +0200

    initial commit and finished with the basic interface of the app

---
+++ SetGoalApp/GoalsVC.swift
@@ -0,0 +1,24 @@
+//
+//  GoalsVC.swift
+//  SetGoalApp
+//
+//  Created by El nino Cholo on 2020/09/04.
+//  Copyright © 2020 El nino Cholo. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC : UIViewController
+{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    @IBAction func addgoas(_ sender: Any)
+    {
+    }
+}
